5d6d69ebdb8be9151c017e7b4b83dd9f2fd2abb3
jeongjiwoo0522 <63524088+jeongjiwoo0522@users.noreply.github.com>
2020-10-11 02:14:39 +0900

    20201011 signup api

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,40 @@
 const express = require('express');
 const router = express.Router();
 
+const bcrypt = require("bcrypt");
+
+const { User } = require("../models");
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post("/signup", async (req, res) => {
+  const { code, id, password, phoneNumber } = req.body;
+  try {
+    const user = await User.findOne({ where: { code } });
+    if(!user) {
+      return res.status(400).json({
+        message: "코드가 일치하지 않습니다.",
+      });
+    } 
+    const hash = await bcrypt.hash(password, 12);
+    await User.update({
+      nick: id,
+      phoneNumber,
+      password: hash,
+    }, {
+      where: { id: user.id },
+    });
+    res.status(200).json({
+      message: "signup", 
+    });
+  } catch(err) {
+    console.error(err);
+    res.status(500).json({
+      message: "서버 에러",
+    });
+  }
 });
 
 module.exports = router;
