a45bc6ab007d513a282000ba7aba68915f54bac1
jeongjiwoo0522 <63524088+jeongjiwoo0522@users.noreply.github.com>
2020-10-12 11:09:15 +0900

    20201012 circles api

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const bcrypt = require("bcrypt");
 
-const { User } = require("../models");
+const { User, Circle, Tag } = require("../models");
 
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
@@ -33,6 +33,24 @@ router.post("/signup", async (req, res) => {
     console.error(err);
     res.status(500).json({
       message: "서버 에러",
+    });
+  }
+});
+
+router.get("/circles", async (req, res) => {
+  try {
+    const circles = await Circle.findAll({
+      include: {
+        model: Tag,
+        attributes: ["title"],
+      },
+    });
+    circles.Tags.map(t => t.title);
+    res.status(200).json(circles);
+  } catch(err) {
+    console.error(err);
+    res.staus(500).json({
+      messge: "서버 에러",
     });
   }
 });
