cfb35e7277938ccc998ba1c252eaa9615d479fc7
jeongjiwoo0522 <63524088+jeongjiwoo0522@users.noreply.github.com>
2020-10-12 18:24:45 +0900

    20201012 login api

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 
 const bcrypt = require("bcrypt");
 
 const { User, Circle, Tag } = require("../models");
 
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.send("index page");
 }); 
 
 router.post("/signup", async (req, res) => {
@@ -17,18 +18,58 @@ router.post("/signup", async (req, res) => {
       return res.status(400).json({
         message: "코드가 일치하지 않습니다.", 
       });
-    } 
+    }   
+    if(user.nick !== null) {
+      return res.status(403).json({
+        message: "이미 가입된 회원입니다.",
+      });
+    }
     const hash = await bcrypt.hash(password, 12);
     await User.update({
-      nick: id,
-      phoneNumber,
+      nick: id, 
       password: hash,
+      phoneNumber,
     }, {
       where: { id: user.id },
     });
     res.status(200).json({
       message: "signup", 
     });
+  } catch(err) {
+    console.error(err);
+    res.status(500).json({
+      message: "서버 에러",
+    });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { id, password } = req.body;
+    const exUser = await User.findOne({ where: { nick: id }});
+    if(!exUser) {
+      return res.status(400).json({
+        message: "아이디 혹은 패스워드가 일치하지 않습니다",
+      });
+    } else {
+      const result = await bcrypt.compare(password, exUser.password);
+      if(!result) {
+        return res.status(400).json({
+          message: "아이디 혹은 패스워드가 일치하지 않습니다",
+        });
+      } 
+      const token = jwt.sign({
+        id: exUser.id,
+        nick: exUser.nick,
+      }, process.env.JWT_SECRET, {
+        expiresIn: "8h", // 유효기간
+        issuer: "ddyzd", // 발급자
+      });
+      res.status(200).json({
+        authorization: token,
+        admin: exUser.adminCircle,
+      });
+    }
   } catch(err) {
     console.error(err);
     res.status(500).json({
