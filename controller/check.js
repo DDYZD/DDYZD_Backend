29d55a17deb856146dc6f044fdf8456b1e5e2ca3
jeongjiwoo0522 <63524088+jeongjiwoo0522@users.noreply.github.com>
2020-10-19 09:26:22 +0900

    20201019 check api

---
+++ controller/check.js
@@ -0,0 +1,32 @@
+const { User } = require("../models");
+
+const checkCode = async (req, res) => {
+  const { code } = req.body;
+  const exCode = await User.findOne({ 
+    where: { code },
+    attributes: ["code"],
+  });
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  if(!exCode) {
+    return res.status(400).json({ message: "없는 코드입니다." });
+  }
+  res.status(200).json({ message: "check" });
+};
+
+const checkId = async (req, res) => {
+  const { code: nick } = req.body;
+  const exCode = await User.findOne({ 
+    where: { nick },
+    attributes: ["nick"],
+  });
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  if(exCode) {
+    return res.status(400).json({ message: "이미 있는 아이디입니다." });
+  }
+  res.status(200).json({ message: "check" });
+};
+
+module.exports = {
+  checkCode,
+  checkId,
+};
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,13 @@ const router = express.Router();
 const { errorHandler } = require("./middleware");
 
 const authController = require("../controller/auth");
+const checkController = require("../controller/check");
 
 const signupRouter = errorHandler(authController.sign);
 const loginRouter = errorHandler(authController.login);
+
+const checkCodeRouter = errorHandler(checkController.checkCode);
+const checkIdRouter = errorHandler(checkController.checkId);
 
 router.get('/', (req, res, next) => {
   res.send("ddyzd api server");
@@ -14,5 +18,8 @@ router.get('/', (req, res, next) => {
 
 router.post("/signup", signupRouter);
 router.post("/login", loginRouter);
+
+router.post("/checkCODE", checkCodeRouter);
+router.post("/checkID", checkIdRouter);
 
 module.exports = router;
